a5373f6ee606d4b25464016cfc0723ebbb000e0b
Arpit Patel <patelarpit2580@gmail.com>
2024-12-27 16:53:03 +0530

    link functionality changes

--- src/components/MyProject/MyLatestWork.jsx
+++ src/components/MyProject/MyLatestWork.jsx
@@ -11,66 +11,76 @@ const MyLatestWork = () => {
         <div id="portfolio" className="container">
             <h2 className="title">My Latest Work</h2>
             <div className="work-grid">
-                <div className="card">
-                    <img src={resume} alt="Resume_Builder" />
-                    <div className="card-content">
-                        <h3>Build professional resumes with tailored templates and expert advice.</h3>
-                        <div className="links">
-                            <a href="https://mavericks-cv.netlify.app/" target="_blank" rel="noopener noreferrer">Live Site</a>
+                <a href="https://mavericks-cv.netlify.app/" target="_blank" rel="noopener noreferrer">
+                    <div className="card">
+                        <img src={resume} alt="Resume_Builder" />
+                        <div className="card-content">
+                            <h3>Build professional resumes with tailored templates and expert advice.</h3>
+                            <div className="links">
+                                <a href="https://mavericks-cv.netlify.app/" target="_blank" rel="noopener noreferrer">Live Site</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="card">
-                    <img src={spotify} alt="spotify" />
-                    <div className="card-content">
-                        <h3>Spotify is a popular music streaming service that allows users to access a vast library of songs, albums, and podcasts.</h3>
-                        <div className="links">
-                            <a href="https://github.com/your-username/your-repository" target="_blank" rel="noopener noreferrer">GitHub</a>
+                </a>
+                <a href="https://github.com/arpit7s/SpotifyClone" target="_blank" rel="noopener noreferrer">
+                    <div className="card">
+                        <img src={spotify} alt="spotify" />
+                        <div className="card-content">
+                            <h3>Spotify is a popular music streaming service that allows users to access a vast library of songs, albums, and podcasts.</h3>
+                            <div className="links">
+                                <a href="https://github.com/arpit7s/SpotifyClone" target="_blank" rel="noopener noreferrer">GitHub</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="card">
-                    <img src={task} alt="task" />
-                    <div className="card-content">
-                        <h3>A task manager is a productivity tool that helps users organize, track, and manage their tasks and responsibilities.</h3>
-                        <div className="links">
-                            <a href="https://github.com/arpit7s/TaskManager" target="_blank" rel="noopener noreferrer">GitHub</a>
-                            <a href="https://fabulous-marigold-3489fc.netlify.app/" target="_blank" rel="noopener noreferrer">Live Site</a>
+                </a>
+                <a href="https://fabulous-marigold-3489fc.netlify.app/" target="_blank" rel="noopener noreferrer">
+                    <div className="card">
+                        <img src={task} alt="task" />
+                        <div className="card-content">
+                            <h3>A task manager is a productivity tool that helps users organize, track, and manage their tasks and responsibilities.</h3>
+                            <div className="links">
+                                <a href="https://github.com/arpit7s/TaskManager" target="_blank" rel="noopener noreferrer">GitHub</a>
+                                <a href="https://fabulous-marigold-3489fc.netlify.app/" target="_blank" rel="noopener noreferrer">Live Site</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="card">
-                    <img src={timer} alt="timer" />
-                    <div className="card-content">
-                        <h3>A timer is a device or software application that counts down or counts up to measure time.</h3>
-                        <div className="links">
-                            <a href="https://github.com/arpit7s/Timer_Application" target="_blank" rel="noopener noreferrer">GitHub</a>
-                            <a href="https://stately-alfajores-51f10f.netlify.app/" target="_blank" rel="noopener noreferrer">Live Site</a>
+                </a>
+                <a href="https://stately-alfajores-51f10f.netlify.app/" target="_blank" rel="noopener noreferrer">
+                    <div className="card">
+                        <img src={timer} alt="timer" />
+                        <div className="card-content">
+                            <h3>A timer is a device or software application that counts down or counts up to measure time.</h3>
+                            <div className="links">
+                                <a href="https://github.com/arpit7s/Timer_Application" target="_blank" rel="noopener noreferrer">GitHub</a>
+                                <a href="https://stately-alfajores-51f10f.netlify.app/" target="_blank" rel="noopener noreferrer">Live Site</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="card">
-                    <img src={quote} alt="quotes" />
-                    <div className="card-content">
-                        <h3>A quote is a statement or expression that is repeated or cited, often because it holds significant meaning, wisdom, or insight.</h3>
-                        <div className="links">
-                            <a href="https://github.com/arpit7s/Quotes" target="_blank" rel="noopener noreferrer">GitHub</a>
-
+                </a>
+                <a href="https://github.com/arpit7s/Quotes" target="_blank" rel="noopener noreferrer">
+                    <div className="card">
+                        <img src={quote} alt="quotes" />
+                        <div className="card-content">
+                            <h3>A quote is a statement or expression that is repeated or cited, often because it holds significant meaning, wisdom, or insight.</h3>
+                            <div className="links">
+                                <a href="https://github.com/arpit7s/Quotes" target="_blank" rel="noopener noreferrer">GitHub</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="card">
-                    <img src={whether} alt="whether" />
-                    <div className="card-content">
-                        <h3>Event-Driven State Chart for Weather Application</h3>
-                        <p>HERO SECTION</p>
-                        <div className="links">
-                            <a href="https://github.com/arpit7s/wheatherappNodejs" target="_blank" rel="noopener noreferrer">GitHub</a>
-
+                </a>
+                <a href="https://github.com/arpit7s/wheatherappNodejs" target="_blank" rel="noopener noreferrer">
+                    <div className="card">
+                        <img src={whether} alt="whether" />
+                        <div className="card-content">
+                            <h3>Event-Driven State Chart for Weather Application</h3>
+                            <div className="links">
+                                <a href="https://github.com/arpit7s/wheatherappNodejs" target="_blank" rel="noopener noreferrer">GitHub</a>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
+
             <button className="get-more-btn">
                 Show More →
             </button>
